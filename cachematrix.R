d1af77aaa3190ba98bb70eb1af9271f44153cf7d
pooja <poojaraju682308@gmail.com>
2018-06-07 00:03:48 +0530

    This version is to be evaluated
    This is the final one

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-## These two functions are all about finding the inverse of matrices and helping to avoid repetition on certain aspects like finding inverse 
+## These two functions are all about finding the 
+## inverse of matrices and helping to avoid repetition
+## on certain aspects like finding inverse 
 
 ##makeCacheMatrix will get a matrix to perform the function
 
@@ -11,13 +13,14 @@ makeCacheMatrix <- function(x = matrix())
     inv <<- NULL
    }
    get <- function() x
-   setInverse <- function(solveMatrix) inv <<- solveMatrix
+   setInverse <- function(inverse) inv <<- inverse
    getInverse <- function() inv
    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
  }
 
 
-## cacheSolve is to find the inverse of the given matrix, if no such matrix is given then it will pass on the previous value to 
+## cacheSolve is to find the inverse of the given matrix,
+##if no such matrix is given then it will pass on the previous value 
 cacheSolve <- function(x, ...)
   {
    ## Return a matrix that is the inverse of 'x'
